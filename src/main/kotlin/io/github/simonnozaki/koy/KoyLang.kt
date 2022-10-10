cb3e59555ea85709d07e63cc33dfbdd858911e64
snozaki <snozaki@netprotections.co.jp>
2022-10-10 21:39:25 +0900

    MOD: make koylang main function to parse program

--- src/main/kotlin/io/github/simonnozaki/koy/KoyLang.kt
+++ src/main/kotlin/io/github/simonnozaki/koy/KoyLang.kt
@@ -1,29 +1,50 @@
 package io.github.simonnozaki.koy
 
+import org.javafp.parsecj.input.Input
 import java.nio.file.Files
 import java.nio.file.Paths
 
+internal val interpreter = Interpreter()
 
-object KoyLang {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        if (args.size != 2) {
-            throw KoyLangRuntimeException("Boot koy application with 2 arguments.")
-        }
+fun main(args: Array<String>) {
+    if (args.size != 2) {
+        throw KoyLangRuntimeException("Boot koy application with 2 arguments.")
+    }
 
-        when (args[0]) {
-            "-f" -> {
-                val fileName = args[1]
-                val path = Paths.get(fileName)
-                println(path)
-                if (!Files.exists(path)) {
-                    throw KoyLangRuntimeException("File [ $fileName ] is not found.")
-                }
-            }
-            else -> System.err.println("""
-            |Usage: java -jar koy.jar -f <fileName>
-            |<fileName>   : read a program from <fileName> and execute it
-            """.trimIndent())
+    when (args[0]) {
+        "-f" -> {
+            val fileName = args[1]
+            val content = getFileContent(fileName)
+
+            val program = Parsers.program().parse(Input.of(content)).result
+            interpreter.callMain(program)
         }
+        else -> System.err.println("""
+        |Usage: java -jar koy.jar -f <fileName>
+        |<fileName>   : read a program from <fileName> and execute it
+        """.trimIndent())
+    }
+
+}
+
+/**
+ * Get file content from file path and name
+ * @param fileName
+ */
+internal fun getFileContent(fileName: String): String {
+    val path = Paths.get(fileName)
+    if (!Files.exists(path)) {
+        throw KoyLangRuntimeException("File [ $fileName ] is not found.")
+    }
+
+    Files.newBufferedReader(path).use {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append(it.readLine())
+        var line = it.readLine()
+        while (line != null) {
+            line = it.readLine()
+            stringBuilder.append(line)
+        }
+        return stringBuilder.toString()
     }
 }
